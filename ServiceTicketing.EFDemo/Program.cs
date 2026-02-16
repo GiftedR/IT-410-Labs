7a95754996381a6229bfac2fd9840046a24c2e6b
GiftedR <kidsgamers@hotmail.com>
2026-02-15 22:52:38 -0800

    info: Added Files

---
+++ ServiceTicketing.EFDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceTicketing.EFDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
